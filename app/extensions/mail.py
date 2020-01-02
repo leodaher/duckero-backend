00534e86c0010e745a97210d41067dcd47a1a2bd
Leonardo Daher <leonardo.daher@usp.br>
2020-01-02 00:54:22 -0300

    Send sign up mail asynchronously

--- app/extensions/mail.py
+++ app/extensions/mail.py
@@ -1,13 +1,22 @@
+from flask import current_app
 from flask_mail import Mail, Message
 
+from app.decorators import async
+
 mail = Mail()
+
+
+@async
+def send_async_mail(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 
 def send_mail(subject, sender, recipients, text_body, html_body):
     msg = Message(
         subject, sender=sender, recipients=recipients, body=text_body, html=html_body
     )
-    mail.send(msg)
+    send_async_mail(current_app._get_current_object(), msg)
 
 
 def send_sign_up_mail(user):
--- tests/test_mail.py
+++ tests/test_mail.py
@@ -1,11 +1,22 @@
 from unittest import mock
+from flask import current_app
 from flask_mail import Message
-from app.extensions.mail import mail, send_mail, send_sign_up_mail
+from app.extensions.mail import mail, send_mail, send_async_mail, send_sign_up_mail
 
 
 @mock.patch("app.extensions.mail.Mail.send")
+def test_send_async_mail(mock_mail_send, faker):
+    app = current_app._get_current_object()
+    msg = Message(faker.sentence())
+
+    send_async_mail(app, msg)
+
+    mock_mail_send.assert_called_once_with(msg)
+
+
+@mock.patch("app.extensions.mail.send_async_mail")
 @mock.patch("app.extensions.mail.Message")
-def test_send_mail(mock_message, mock_mail_send, faker):
+def test_send_mail(mock_message, mock_send_async_mail, faker):
     subject = faker.sentence()
     sender = faker.email()
     recipients = [faker.email(), faker.email()]
@@ -22,7 +33,7 @@ def test_send_mail(mock_message, mock_mail_send, faker):
     mock_message.assert_called_once_with(
         subject, sender=sender, recipients=recipients, body=body, html=html
     )
-    mock_mail_send.assert_called_once_with(msg)
+    mock_send_async_mail.assert_called_once_with(current_app._get_current_object(), msg)
 
 
 @mock.patch("app.extensions.mail.send_mail")
